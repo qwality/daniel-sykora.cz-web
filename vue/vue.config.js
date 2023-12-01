5541dd4aecb3dcb9c348020077bb02d2cc9562a1
qwality <low.qwality@gmail.com>
2023-12-01 21:17:39 +0100

    rfewrewfw

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -3,4 +3,5 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   publicPath: 'https://daniel-sykora.cz/vue',
   transpileDependencies: true,
+  allowedHosts: ['daniel-sykora.cz'],
 });
