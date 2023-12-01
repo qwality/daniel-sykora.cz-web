c7eecb3f5dc154aefbe68f0ce2e16d0f2eb8bc94
qwality <low.qwality@gmail.com>
2023-12-01 20:59:48 +0100

    ffdsfdsf

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,9 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    disableHostCheck: true, // nebo použijte allowedHosts pro konkrétní hostitele
-    // allowedHosts: ['váš.domenový.název', 'další.povolený.host']
-  }
+  transpileDependencies: true
 });
