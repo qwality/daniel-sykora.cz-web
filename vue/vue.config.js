8b14572cbe1bc6c691b85c5272e66f50a33c1135
qwality <low.qwality@gmail.com>
2023-12-01 21:03:48 +0100

    fdsfdsf

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    public: 'daniel-sykora.cz',
+  "transpileDependencies": true,
+  "devServer": {
+    "public": "daniel-sykora.cz",
   }
 });
