6bebebd4becabc93a6897bbb69d703b9cae25c99
qwality <low.qwality@gmail.com>
2023-12-04 15:47:25 +0100

    trterte

--- vite_vue/vite.config.js
+++ vite_vue/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/vite/dist/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/vite/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
