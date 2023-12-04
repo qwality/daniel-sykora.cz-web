830920ce1b639a0379da3fefc862e2a98f14f291
qwality <low.qwality@gmail.com>
2023-12-04 15:40:42 +0100

    fdsfsd

--- vite_vue/vite.config.js
+++ vite_vue/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/vite_vue/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/vite_vue/dist/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
