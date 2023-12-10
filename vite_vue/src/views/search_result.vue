22886d1146acef93c9eb630a84b46417dbc3c298
qwality <low.qwality@gmail.com>
2023-12-10 20:38:32 +0100

    typo

--- vite_vue/src/views/search_result.vue
+++ vite_vue/src/views/search_result.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="card shadow-xl bg-base-100 min-w-full hover:opacity-90">
       <div class="card-body flex flex-row gap-4 flex-wrap">
-        <h2 class="card-title self-start">Search is not jet implemented !</h2>
+        <h2 class="card-title self-start">Search is not yet implemented !</h2>
         <div class="card shadow-xl bg-neutral hover:opacity-90 grow">
             <div class="card-body flex flex-row">
                 <h2 class="text-secondary font-semibold">Your query was:</h2>
