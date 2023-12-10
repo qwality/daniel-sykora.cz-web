bd14240f3f6ade71853aa40e3d34a89640d89ff4
qwality <low.qwality@gmail.com>
2023-12-10 20:31:12 +0100

    dsadasd

--- vite_vue/src/views/sbirka_znalosti.vue
+++ vite_vue/src/views/sbirka_znalosti.vue
@@ -1,7 +1,48 @@
 <template>
-    <div>
-        <h1>sbirka znalosti</h1>
+  <div class="grid-card-layout w-1/2">
+    <!-- python -->
+    <div class="grid-small-card">
+      <div class="card-body">
+        <h2 class="card-title text-secondary">python</h2>
+        <img class=" ml-2 w-4 h-4 self-center" src="/img/python.png">
+      </div>
     </div>
+    <!-- javascript -->
+    <div class="grid-small-card">
+      <div class="card-body">
+        <h2 class="card-title text-secondary">javascript</h2>
+        <img class=" ml-2 w-4 h-4 self-center" src="/img/javascript.png">
+      </div>
+    </div>
+    <!-- HTML -->
+    <div class="grid-small-card">
+      <div class="card-body">
+        <h2 class="card-title text-secondary">HTML</h2>
+        <img class=" ml-2 w-4 h-4 self-center" src="/img/html.png">
+      </div>
+    </div>
+    <!-- CSS -->
+    <div class="grid-small-card">
+      <div class="card-body">
+        <h2 class="card-title text-secondary"><i>CSS </i></h2>
+        <img class=" ml-2 w-4 h-4 self-center" src="/img/css.png">
+      </div>
+    </div>
+    <!-- FastAPI -->
+    <div class="grid-small-card">
+      <div class="card-body">
+        <h2 class="card-title text-secondary">FastAPI</h2>
+        <img class=" ml-2 w-4 h-4 self-center" src="/img/fastapi.svg">
+      </div>
+    </div>
+    <!-- Vue -->
+    <div class="grid-small-card">
+      <div class="card-body">
+        <h2 class="card-title text-secondary">Vue</h2>
+        <img class=" ml-2 w-4 h-4 self-center" src="/img/vue.png">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,4 +52,10 @@ export default {
 </script>
 
 <style scoped>
+.grid-card-layout{
+  @apply flex m-auto justify-center;
+}
+.grid-small-card{
+  @apply card shadow-xl bg-base-100 hover:opacity-90 min-w-min max-w-max;
+}
 </style>
