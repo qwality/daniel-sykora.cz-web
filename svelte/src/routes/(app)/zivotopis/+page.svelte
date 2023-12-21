641309ba73bf1c5334668074498f60e066e504b8
qwality <low.qwality@gmail.com>
2023-12-21 19:49:27 +0100

    Update print button and add PDF download link

--- svelte/src/routes/(app)/zivotopis/+page.svelte
+++ svelte/src/routes/(app)/zivotopis/+page.svelte
@@ -1,8 +1,15 @@
 <Zivotopis />
-<!-- Prin button -->
-<a href="{base}/print_zivotopis" class="btn btn-secondary w-min px-8 text-lg whitespace-nowrap col-span-full ml-auto mt-auto">
-    Printable version
-</a>
+<div class="ml-auto mt-auto join gap-1 flex-nowrap flex">
+    <a href="https://daniel-sykora.cz/uvicorn/static/pdf/daniel-sykora-developer.pdf" class="btn btn-secondary join-item w-min px-8 text-lg whitespace-nowrap col-span-full">
+        PDF
+    </a>
+    <!-- Prin button -->
+    <a href="{base}/print_zivotopis" class="btn btn-secondary join-item w-min px-8 text-lg whitespace-nowrap col-span-full ">
+        Printable version
+    </a>
+</div>
+
+
 <script>
     import {base} from '$app/paths';
     import Zivotopis from "$lib/zivotopis.svelte";
